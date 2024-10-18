f421898344600367a3a4250c2d440d83ff8f6b6b
fixed27 <gilbertodamian14@gmail.com>
2024-10-17 22:19:42 -0600

    feat: add some things

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("create", views.question_create, name="create"),
+    path("<int:question_id>", views.question_view, name="view"),
 ]
